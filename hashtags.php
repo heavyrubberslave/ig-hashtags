74c710641d9546daa0474be7f33960addc01906a
heavyrubberslave <pascal@heavyrubberslave.com>
2019-09-01 09:26:15 +0200

    Add PHP files

---
+++ hashtags.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace HashtagValidator;
+
+class HashtagValidator
+{
+
+    private const HASHTAG_PATTERN = '/(?:^|\s)?([#＃][\w\x{05be}\x{05f3}\x{05f4}]*[\p{L}_]+[\w\x{05be}\x{05f3}\x{05f4}]*)/um';
+
+    private $hashtagBlacklist;
+
+    private $hashtagWhitelist;
+
+    public function __construct(array $hashtagBlacklist, array $hashtagWhitelist = [])
+    {
+        $this->hashtagBlacklist = $hashtagBlacklist;
+        $this->hashtagWhitelist = $hashtagWhitelist;
+    }
+
+    public function validate(string $hashtags): array
+    {
+        $matches = null;
+        $matchCount = preg_match_all(self::HASHTAG_PATTERN, $hashtags, $matches);
+
+        if (false === $matchCount) {
+            $pregError = array_flip(get_defined_constants(true)['pcre'])[preg_last_error()];
+            throw new \InvalidArgumentException('Could not parse hashtags: ' . $pregError);
+        }
+
+        $parsedHashtags = $matches[1];
+        $validHashtags = [];
+        $blockedHashtags = [];
+        $riskyHashtags = [];
+        $approvedHashtags = [];
+
+        foreach ($parsedHashtags as $hashtag) {
+            $hashtagWithoutHash = substr($hashtag, 1);
+
+            if (true === in_array($hashtagWithoutHash, $this->hashtagBlacklist, true)) {
+                $blockedHashtags[] = $hashtag;
+            } else {
+                $cssClass = 'badge-light';
+
+                foreach ($this->hashtagBlacklist as $blockedHashtag) {
+                    if (false !== strpos($hashtag, $blockedHashtag)) {
+                        $riskyHashtags[] = $hashtag;
+                        $cssClass = 'badge-warning';
+                        break;
+                    }
+                }
+
+                if (isset($this->hashtagWhitelist[$hashtagWithoutHash])) {
+                    $approvedHashtags[] = $hashtag;
+                    $cssClass = 'badge-primary';
+                }
+
+                $validHashtags[] = '<span class="badge ' . $cssClass . '">' . $hashtag . '</span>';
+            }
+        }
+
+        return ['valid' => $validHashtags, 'banned' => $blockedHashtags, 'risky' => $riskyHashtags, 'approved' => $approvedHashtags];
+    }
+}
+
+$hashtagBlacklist = (function() {
+    $content = file_get_contents('https://raw.githubusercontent.com/heavyrubberslave/ig-hashtags/master/banned');
+    return preg_split("/\s+/", $content, -1, PREG_SPLIT_NO_EMPTY);
+})();
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hashtag validator</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" crossorigin="anonymous">
+        <style type="text/css">
+            textarea.form-control {
+                height: 150px;
+            }
+
+            .error {
+                color: #c00;
+            }
+
+            .hashtags dt {
+                font-weight: bold;
+            }
+
+            .hashtags-blocked .card-header,
+            .hashtags-valid .card-header {
+                font-size: 1.5em;
+            }
+
+            .hashtags-blocked .card-header {
+                color: #c00;
+            }
+
+            .hashtags-valid .card-header {
+                color: green;
+            }
+
+            .risky {
+                color: orange;
+            }
+
+            .approved {
+                color: blue;
+            }
+
+            .hashtags dd {
+                margin: 0;
+                padding: 0;
+            }
+
+            .note {
+                color: #666;
+                font-size: 0.7em;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-dark bg-dark border-bottom box-shadow mb-3">
+                <div class="container">
+                    <a class="navbar-brand" href="/">Hashtag validator</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+                        <ul class="navbar-nav flex-grow-1">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="container">
+            <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+                <div class="form-group">
+                    <textarea class="form-control" name="hashtags" placeholder="Enter hashtags to validate..."></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Validate</button>
+            </form>
+
+            <?php
+            if (isset($_POST['hashtags'])):
+                $hashtagWhitelist = require_once __DIR__ . '/hashtags_whitelist.php';
+
+                $hashtagValidator = new HashtagValidator($hashtagBlacklist, $hashtagWhitelist);
+
+                try {
+                    $result = $hashtagValidator->validate(strip_tags($_POST['hashtags']));
+                    $validHashtags = $result['valid'];
+                    $blockedHashtags = $result['banned'];
+                    $riskyHashtags = $result['risky'];
+                } catch (\Exception $e) {
+                    echo '<p class="error"><b>' , $e->getMessage() , '</b></p>';
+                }
+            ?>
+            <hr>
+            <div class="hashtags-valid card my-3">
+                <div class="card-header">
+                    Valid hashtags <span class="badge badge-success float-right"><?php echo count($validHashtags); ?></span>
+                </div>
+                <div class="card-body">
+                    <p id="valid-hashtag-content"><?php echo implode(' ', $validHashtags); ?></p>
+                    <p><button class="js-copy-to-clipboard btn btn-secondary" data-clipboard-target="#valid-hashtag-content"><i class="fas fa-copy"></i> Copy valid hashtags to clipboard</button></p>
+                    <p class="note card-text"><span class="badge badge-warning">Yellow hashtags</span> are considered risky. This means they're not currently on the blacklist but chances are higher that they may be blocked. Check any yellow hashtags and report them to me if they're actually blocked.</p>
+                    <p class="note card-text"><span class="badge badge-primary">Blue hashtags</span> are approved and seem to be not banned at the moment they were checked and approved.</p>
+                </div>
+            </div>
+
+            <div class="hashtags-blocked card my-3">
+                <div class="card-header">
+                    Banned hashtags <span class="badge badge-danger float-right"><?php echo count($blockedHashtags); ?></span>
+                </div>
+                <div class="card-body">
+                    <p class="card-text"><?php echo count($blockedHashtags) > 0 ? '<span class="badge badge-danger">' . implode('</span> <span class="badge badge-danger">', $blockedHashtags) . '</span>' : 'No banned hashtags were found.'; ?></p>
+                </div>
+            </div>
+            <?php endif; ?>
+
+            <hr>
+
+            <h2>Blacklist</h2>
+            <p>Currently there are <b><?php echo count($hashtagBlacklist); ?></b> blocked hashtags.</p>
+
+            <p>
+                <a class="btn btn-outline-secondary" data-toggle="collapse" href="#bannedHashtagList" role="button" aria-expanded="false" aria-controls="bannedHashtagList">Show complete list</a>
+            </p>
+            <div class="collapse" id="bannedHashtagList">
+                <div class="card card-body">
+                    <p><?php natsort($hashtagBlacklist); echo implode(' ', $hashtagBlacklist); ?></p>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://cdn.rawgit.com/zenorocha/clipboard.js/v2.0.4/dist/clipboard.min.js"></script>
+        <script type="text/javascript">
+            var clip = new ClipboardJS('.js-copy-to-clipboard');
+        </script>
+    </body>
+</html>
---
+++ hashtags_whitelist.php
@@ -0,0 +1,885 @@
+<?php
+
+return array (
+  'collar' => 
+  array (
+    'categories' => 
+    array (
+      'collar'
+    ),
+  ),
+  'latexmask' => 
+  array (
+    'categories' => 
+    array (
+      'mask', 'hood', 'latex', 'rubber'
+    ),
+  ),
+  'rubberfun' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex'
+    ),
+  ),
+  'rubberlover' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex'
+    ),
+  ),
+  'pinklips' => 
+  array (
+    'categories' => 
+    array (
+      'lips'
+    ),
+  ),
+  'alternativegirl' => 
+  array (
+    'categories' => 
+    array (
+      'female'
+    ),
+  ),
+  'highheelshoes' => 
+  array (
+    'categories' => 
+    array (
+      'shoes', 'boots'
+    ),
+  ),
+  'rubberskin' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex'
+    ),
+  ),
+  'rubbergirl' => 
+  array (
+    'categories' => 
+    array (
+      'female', 'rubber', 'latex'
+    ),
+  ),
+  'altmodel' => 
+  array (
+    'categories' => 
+    array (
+      'model'
+    ),
+  ),
+  'rubberfetish' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'rubbermodel' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'model',
+    ),
+  ),
+  'neoprenebody' => 
+  array (
+    'categories' => 
+    array (
+      'neoprene',
+    ),
+  ),
+  'neoprenefetish' => 
+  array (
+    'categories' => 
+    array (
+      'neoprene',
+    ),
+  ),
+  'rubberdolls' => 
+  array (
+    'categories' => 
+    array (
+      'doll', 'latex', 'rubber',
+    ),
+  ),
+  'rubber' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber'
+    ),
+  ),
+  'heavyrubberfetish' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex',
+    ),
+  ),
+  'gasmaskfetish' => 
+  array (
+    'categories' => 
+    array (
+      'gasmask'
+    ),
+  ),
+  'leatherboots' => 
+  array (
+    'categories' => 
+    array (
+      'boots', 'shoes', 'leather',
+    ),
+  ),
+  'leatherpants' => 
+  array (
+    'categories' => 
+    array (
+      'leather',
+    ),
+  ),
+  'longlegs' => 
+  array (
+    'categories' => 
+    array (
+      'legs'
+    ),
+  ),
+  'shoelovers' => 
+  array (
+    'categories' => 
+    array (
+      'shoes', 'boots',
+    ),
+  ),
+  'shoefetish' => 
+  array (
+    'categories' => 
+    array (
+      'shoes', 'boots',
+    ),
+  ),
+  'leatherfetishgirl' => 
+  array (
+    'categories' => 
+    array (
+      'leather', 'female',
+    ),
+  ),
+  'kinkychicks' => 
+  array (
+    'categories' => 
+    array (
+      'female',
+    ),
+  ),
+  'gasmask' => 
+  array (
+    'categories' => 
+    array (
+      'gasmask',
+    ),
+  ),
+  'latexglovesfetish' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'gloves', 'rubber',
+    ),
+  ),
+  'ballgaggegirl' => 
+  array (
+    'categories' => 
+    array (
+      'female', 'gag',
+    ),
+  ),
+  'latexhood' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'hood', 'rubber',
+    ),
+  ),
+  'sexylips' => 
+  array (
+    'categories' => 
+    array (
+      'lips',
+    ),
+  ),
+  'girlinlatex' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'feamle'
+    ),
+  ),
+  'rubberist' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'mistress' => 
+  array (
+    'categories' => 
+    array (
+      'mistress', 'female',
+    ),
+  ),
+  'latexglovefetish' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber', 'gloves',
+    ),
+  ),
+  'sadisticgirls' => 
+  array (
+    'categories' => 
+    array (
+      'mistress', 'female', 'sm',
+    ),
+  ),
+  'bootsfetish' => 
+  array (
+    'categories' => 
+    array (
+      'shoes', 'boots',
+    ),
+  ),
+  'highheelslover' => 
+  array (
+    'categories' => 
+    array (
+      'shoes', 'boots',
+    ),
+  ),
+  'collaredwomen' => 
+  array (
+    'categories' => 
+    array (
+      'female', 'collar',
+    ),
+  ),
+  'dominatrix' => 
+  array (
+    'categories' => 
+    array (
+      'mistress', 'female',
+    ),
+  ),
+  'corset' => 
+  array (
+    'categories' => 
+    array (
+      'corset',
+    ),
+  ),
+  'feetworship' => 
+  array (
+    'categories' => 
+    array (
+      'feet'
+    ),
+  ),
+  'goddess' => 
+  array (
+    'categories' => 
+    array (
+      'mistress', 'female',
+    ),
+  ),
+  'catsuit' => 
+  array (
+    'categories' => 
+    array (
+      'catsuit',
+    ),
+  ),
+  'higheels' => 
+  array (
+    'categories' => 
+    array (
+      'shoes', 'boots'
+    ),
+  ),
+  'highheels' => 
+  array (
+    'categories' => 
+    array (
+      'shoes', 'boots',
+    ),
+  ),
+  'altmodels' => 
+  array (
+    'categories' => 
+    array (
+      'model',
+    ),
+  ),
+  'latexselfie' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber', 'selfie',
+    ),
+  ),
+  'heavyrubber' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex',
+    ),
+  ),
+  'redlips' => 
+  array (
+    'categories' => 
+    array (
+      'lips',
+    ),
+  ),
+  'leatherdress' => 
+  array (
+    'categories' => 
+    array (
+      'leather', 'female'
+    ),
+  ),
+  'leatherfashion' => 
+  array (
+    'categories' => 
+    array (
+      'leather',
+    ),
+  ),
+  'leathercollars' => 
+  array (
+    'categories' => 
+    array (
+      'leather', 'collar',
+    ),
+  ),
+  'shooting' => 
+  array (
+    'categories' => 
+    array (
+      'model',
+    ),
+  ),
+  'latexbubbles' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'layeredlatex' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'latexcouple' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber', 'couple',
+    ),
+  ),
+  'kinkyshit' => 
+  array (
+    'categories' => 
+    array (
+      'kink',
+    ),
+  ),
+  'electrified' => 
+  array (
+    'categories' => 
+    array (
+      'estim',
+    ),
+  ),
+  'latexswimwear' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'femaledomination' => 
+  array (
+    'categories' => 
+    array (
+      'mistress', 'female',
+    ),
+  ),
+  'latexpants' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'pleasershoes' => 
+  array (
+    'categories' => 
+    array (
+      'boots', 'shoes',
+    ),
+  ),
+  'blondedominatrix' => 
+  array (
+    'categories' => 
+    array (
+      'mistress', 'female',
+    ),
+  ),
+  'heels' => 
+  array (
+    'categories' => 
+    array (
+      'shoes', 'boots',
+    ),
+  ),
+  'leather' => 
+  array (
+    'categories' => 
+    array (
+      'leahter',
+    ),
+  ),
+  'blacknails' => 
+  array (
+    'categories' => 
+    array (
+      'nails',
+    ),
+  ),
+  'classywomen' => 
+  array (
+    'categories' => 
+    array (
+      'female',
+    ),
+  ),
+  'latexboobs' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber', 'female',
+    ),
+  ),
+  'rednails' => 
+  array (
+    'categories' => 
+    array (
+      'nails',
+    ),
+  ),
+  'marks' => 
+  array (
+    'categories' => 
+    array (
+      'sm',
+    ),
+  ),
+  'whip' => 
+  array (
+    'categories' => 
+    array (
+    ),
+  ),
+  'sadistic' => 
+  array (
+    'categories' => 
+    array (
+      'sm',
+    ),
+  ),
+  'sadism' => 
+  array (
+    'categories' => 
+    array (
+      'sm',
+    ),
+  ),
+  'gasmaskgirl' => 
+  array (
+    'categories' => 
+    array (
+      'gasmask', 'female',
+    ),
+  ),
+  'kinkygirlsbelike' => 
+  array (
+    'categories' => 
+    array (
+      'female', 'kinky',
+    ),
+  ),
+  'redlipstick' => 
+  array (
+    'categories' => 
+    array (
+      'lips', 'female',
+    ),
+  ),
+  'casuallatex' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'hunterboots' => 
+  array (
+    'categories' => 
+    array (
+      'boots', 'latex', 'rubber',
+    ),
+  ),
+  'rainboots' => 
+  array (
+    'categories' => 
+    array (
+      'boots', 'latex', 'rubber',
+    ),
+  ),
+  'everydayfetish' => 
+  array (
+    'categories' => 
+    array (
+      'fetish',
+    ),
+  ),
+  'leatherleggings' => 
+  array (
+    'categories' => 
+    array (
+      'leather',
+    ),
+  ),
+  'bdsmcommunity' => 
+  array (
+    'categories' => 
+    array (
+      'bdsm',
+    ),
+  ),
+  'steel' => 
+  array (
+    'categories' => 
+    array (
+      'steel',
+    ),
+  ),
+  'kinkygirl' => 
+  array (
+    'categories' => 
+    array (
+      'female', 'kinky',
+    ),
+  ),
+  'leathergloves' => 
+  array (
+    'categories' => 
+    array (
+      'gloves', 'leather',
+    ),
+  ),
+  'leatherhood' => 
+  array (
+    'categories' => 
+    array (
+      'hood', 'leather',
+    ),
+  ),
+  'owned' => 
+  array (
+    'categories' => 
+    array (
+      'bdsm',
+    ),
+  ),
+  'kinkyinpublic' => 
+  array (
+    'categories' => 
+    array (
+      'kinky', 'public',
+    ),
+  ),
+  'bdsminpublic' => 
+  array (
+    'categories' => 
+    array (
+      'bdsm', 'public',
+    ),
+  ),
+  'rubbered' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'boots' => 
+  array (
+    'categories' => 
+    array (
+      'boots',
+    ),
+  ),
+  'legs' => 
+  array (
+    'categories' => 
+    array (
+      'legs',
+    ),
+  ),
+  'leggings' => 
+  array (
+    'categories' => 
+    array (
+      'legs'
+    ),
+  ),
+  'masochistic' => 
+  array (
+    'categories' => 
+    array (
+      'sm',
+    ),
+  ),
+  'cane' => 
+  array (
+    'categories' => 
+    array (
+      'sm', 'bdsm',
+    ),
+  ),
+  'gethurt' => 
+  array (
+    'categories' => 
+    array (
+      'sm', 'bdsm',
+    ),
+  ),
+  'torture' => 
+  array (
+    'categories' => 
+    array (
+      'sm', 'bdsm',
+    ),
+  ),
+  'overknees' => 
+  array (
+    'categories' => 
+    array (
+      'boots',
+    ),
+  ),
+  'kneeling' => 
+  array (
+    'categories' => 
+    array (
+      'bdsm',
+    ),
+  ),
+  'corsets' => 
+  array (
+    'categories' => 
+    array (
+      'corset',
+    ),
+  ),
+  'ladyinblack' => 
+  array (
+    'categories' => 
+    array (
+      'female', 'leather', 'latex',
+    ),
+  ),
+  'fullyrubbered' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'overkneeboots' => 
+  array (
+    'categories' => 
+    array (
+      'boots',
+    ),
+  ),
+  'latexinpublic' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'public',
+    ),
+  ),
+  'mask' => 
+  array (
+    'categories' => 
+    array (
+      'hood', 'mask',
+    ),
+  ),
+  'hood' => 
+  array (
+    'categories' => 
+    array (
+      'hood', 'mask',
+    ),
+  ),
+  'feet' => 
+  array (
+    'categories' => 
+    array (
+      'feet',
+    ),
+  ),
+  'barefeet' => 
+  array (
+    'categories' => 
+    array (
+      'feet',
+    ),
+  ),
+  'feetfetishism' => 
+  array (
+    'categories' => 
+    array (
+      'feet',
+    ),
+  ),
+  'rubbershoes' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'boots', 'shoes',
+    ),
+  ),
+  'plateauboots' => 
+  array (
+    'categories' => 
+    array (
+      'boots',
+    ),
+  ),
+  'shiny' => 
+  array (
+    'categories' => 
+    array (
+      'latex', 'rubber',
+    ),
+  ),
+  'switcher' => 
+  array (
+    'categories' => 
+    array (
+      'bdsm', 'sm',
+    ),
+  ),
+  'sleepsack' => 
+  array (
+    'categories' => 
+    array (
+      'sleepsack', 'rubber', 'latex', 'leather',
+    ),
+  ),
+  'sleepbag' => 
+  array (
+    'categories' => 
+    array (
+      'sleepsack', 'rubber', 'latex', 'leather',
+    ),
+  ),
+  'playtime' => 
+  array (
+    'categories' => 
+    array (
+      'bdsm', 'sm',
+    ),
+  ),
+  'bdsmlifestyle' => 
+  array (
+    'categories' => 
+    array (
+      'bdsm', 'sm',
+    ),
+  ),
+  'rubberman' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'male',
+    ),
+  ),
+  'rubbermen' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'male',
+    ),
+  ),
+  'rubbergay' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'male',
+    ),
+  ),
+  'rubberpride' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex',
+    ),
+  ),
+  'rubberboy' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'male',
+    ),
+  ),
+  'rubberboys' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'male',
+    ),
+  ),
+  'rubbergimp' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex',
+    ),
+  ),
+  'rubberguys' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'male',
+    ),
+  ),
+  'rubberguy' => 
+  array (
+    'categories' => 
+    array (
+      'rubber', 'latex', 'male',
+    ),
+  ),
+);
